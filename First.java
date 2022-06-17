14470aaac27b5ff387bfcec6983d5c71aec8a392
ReetuChauhann <chauhanreetu00@gmail.com>
2022-06-17 11:40:39 +0530

    Use of List.of operator in java

---
+++ First.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class First {
+    public static void main(String[] args) {
+        List<Integer> list= List.of(1,2,3,4);
+        System.out.println(list);
+    }
+    
+}
